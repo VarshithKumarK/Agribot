e3104c8838fdab8edc32fa5b7f9051488b1c416d
Varshith Kumar K <varshithkumark983@gmail.com>
2025-04-30 18:52:04 +0530

    second commit

---
+++ backend/models/User.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    favorites: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Music",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+const User = mongoose.model("User", userSchema);
+export default User;
